ba1be7a95a41eba7e6efe9a24643b1fa67b8e916
Th3roo <inzentero@gmail.com>
2024-12-13 01:26:47 +0500

    added saving logs to file

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from src.bot.bot import bot
 # Logger setup
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.INFO,
+    filename='logs/bot.log',
+    filemode='a'
 )
 logger = logging.getLogger(__name__)
 
